bb9c51d65fbc34b59e9d5d6503470270b18a1d9e
educhin <c.s.arinze@gmail.com>
2017-10-08 15:41:23 -0400

    Added collection view table on new view controller. Implemented basic design and attached IBOulets. No bugs presentno identifers to trigger segues

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Chinedu Arinze on 10/8/17.
+//  Copyright © 2017 Chinedu Arinze. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
